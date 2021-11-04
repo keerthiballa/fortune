1a2a5140d8cf2f4f04e47bfdd3ff8995fee33a66
Balla Keerthi <keerthiballa@gmail.com>
2021-11-04 13:53:59 -0400

    Corrected str_ind value

--- fortune.py
+++ fortune.py
@@ -13,7 +13,7 @@ to run it, type in a shell, python3 fortune.py
 file_content = open('fortunedat.txt','r')
 content_str = file_content.read()
 quotes_list = content_str.split("%\n")
-str_ind= len(quotes_list)-1
+str_ind= len(quotes_list)
 
 #print(str_ind)
 
